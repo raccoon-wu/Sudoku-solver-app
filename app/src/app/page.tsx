c51daac413b7cb926b73fb3fce365757bd2567f9
Amy <amywu177@gmail.com>
2025-06-20 00:14:38 +1000

    built user input
    allows users to enter puzzles manually, stores in 2D array

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
+
+const SIZE: number = 9;
+type SudokuGrid = number[][];
+
+// creates empty list with SIZE slots and fills all spots with 0
+// for each slot in the list, make a new list of SIZE number of 0
+const initialGrid: SudokuGrid = new Array(SIZE).fill(0).map(() => //initially .fill(0) just to give .map() something to loop over
+  new Array(SIZE).fill(0)
+);
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [grid, setGrid] = useState<SudokuGrid>(initialGrid);
+  const [message, setMessage] = useState<String>("Fill in sudoku puzzle, then click 'SOLVE'!");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // tracks initial numbers provided by the Sudoku puzzle
+  const [userInput, setUserInput] = useState<boolean[][]>(
+    new Array(SIZE).fill(0).map(() =>
+      new Array(SIZE).fill(false)
+    )
+  );
+
+  const handleNumInput = (row: number, col: number, value: string) => {
+
+    //converts string into integer
+    const num = parseInt(value);
+    if (isNaN(num) || num < 0 || num > 9) return (setMessage("Invalid input!"));
+
+    //makes copy so the original is not mutated
+    const newGrid = grid.map((r) => [...r]);
+    newGrid[row][col] = num;
+
+    const newUserInput = userInput.map((r) => [...r]);
+    newUserInput[row][col] = true;
+
+    setGrid(newGrid);
+    setUserInput(newUserInput);
+
+    console.clear();
+    console.log("Updated puzzle:");
+    console.table(newGrid);
+  }
+
+  return (
+    <div className=" bg-stone-800 w-screen h-screen flex flex-col justify-center items-center">
+      <p className="mb-5 text-4xl text-stone-200">SUDOKU SOLVER</p>
+            <p className="h-12 text-stone-300">{message} </p>
+      <div className="bg-stone-500" style={{ display: "grid", gridTemplateColumns: "repeat(9, 40px)" }}>
+        {grid.map((row, i) =>
+          row.map((cell, j) => (
+            <input
+              key={`${i}-${j}`}
+              type="text"
+              pattern="[0-9]*" //limits user typing input to 0-9, but doesn't stop pasting or '123'
+              value={cell === 0 ? "0" : cell}
+              onChange={(e) => handleNumInput(i, j, e.target.value)}
+              className={`text-center border-1 box-content text-white
+                ${userInput[i][j] ? "bg-stone-500 " : "bg-stone-400"
+                }`}
+              style={{
+                width: "38px",
+                height: "38px",
+              }}>
+
+            </input>
+          ))
+        )}
+      </div>
+      <button className="bg-rose-400 h-12 w-1/4 mt-5 rounded-full cursor-pointer">SOLVE</button>
     </div>
   );
 }
